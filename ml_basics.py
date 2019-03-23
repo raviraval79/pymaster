ee60ac8612db0c89433bfadc4db4c9b5560b1c87
raviraval79 <rraval@gmail.com>
2019-03-23 12:02:12 +0000

    added code to show charts for each column

--- ml_basics.py
+++ ml_basics.py
@@ -1,14 +1,28 @@
+#basic program to load a data and show line chart for the same
+
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
 from sklearn import datasets
 
+#change the file path for trying different data files
 df = pd.read_csv('data.csv')
 
+#uncomment below to inspect data
 #print 'shape - ',df.shape
 #print 'dataset description ---',df.columns
 
+#use below code to create line charts for each column
+#df.plot(kind='line', subplots=True, sharex=False, sharey=False)
 
-df.plot(kind='line', subplots=True, sharex=False, sharey=False)
+#use below code to create histogram for each column
+#df.hist()
+
+#use below code to show a chart for one of the columns
+series = np.array(df['High'])
+plt.plot(series)
+
+
 plt.show()
 
